2be1921cfa36335217d39e4ef8724ac612de78f3
Andrea Bedini <andrea@andreabedini.com>
2026-03-03 17:02:48 +0800

    fix(govc): encrypt VM when adding TPM device
    VMs must be encrypted before a VirtualTPM can be added. Replace the
    simple AddDevice call with a single Reconfigure that sets both Crypto
    (CryptoSpecEncrypt) and DeviceChange in one operation.
    
    A -kms-provider flag selects the KMS provider; if omitted, the cluster
    default is resolved via GetDefaultKmsClusterID. An empty KeyId causes
    vSphere to auto-generate a key for the provider.

--- cli/device/tpm/add.go
+++ cli/device/tpm/add.go
@@ -11,11 +11,13 @@ import (
 
 	"github.com/vmware/govmomi/cli"
 	"github.com/vmware/govmomi/cli/flags"
+	"github.com/vmware/govmomi/crypto"
 	"github.com/vmware/govmomi/vim25/types"
 )
 
 type add struct {
 	*flags.VirtualMachineFlag
+	kmsProvider string
 }
 
 func init() {
@@ -25,13 +27,18 @@ func init() {
 func (cmd *add) Register(ctx context.Context, f *flag.FlagSet) {
 	cmd.VirtualMachineFlag, ctx = flags.NewVirtualMachineFlag(ctx)
 	cmd.VirtualMachineFlag.Register(ctx, f)
+	f.StringVar(&cmd.kmsProvider, "kms-provider", "", "KMS provider ID (default: cluster default)")
 }
 
 func (cmd *add) Description() string {
 	return `Add Trusted Platform Module (TPM) device to VM.
 
+The VM must be encrypted; if it is not already, it will be encrypted using
+the specified KMS provider (or the cluster default if -kms-provider is omitted).
+
 Examples:
   govc device.tpm.add -vm $vm
+  govc device.tpm.add -vm $vm -kms-provider my-kp
   govc device.info tpm-*`
 }
 
@@ -45,10 +52,40 @@ func (cmd *add) Run(ctx context.Context, f *flag.FlagSet) error {
 		return flag.ErrHelp
 	}
 
-	d := &types.VirtualTPM{}
+	providerID := cmd.kmsProvider
+	if providerID == "" {
+		m, err := crypto.GetManagerKmip(vm.Client())
+		if err != nil {
+			return err
+		}
+		providerID, err = m.GetDefaultKmsClusterID(ctx, nil, true)
+		if err != nil {
+			return err
+		}
+		if providerID == "" {
+			return fmt.Errorf("no KMS provider configured; use -kms-provider or configure a default KMS cluster")
+		}
+	}
 
-	err = vm.AddDevice(ctx, d)
+	spec := types.VirtualMachineConfigSpec{
+		Crypto: &types.CryptoSpecEncrypt{
+			CryptoKeyId: types.CryptoKeyId{
+				ProviderId: &types.KeyProviderId{Id: providerID},
+			},
+		},
+		DeviceChange: []types.BaseVirtualDeviceConfigSpec{
+			&types.VirtualDeviceConfigSpec{
+				Device:    &types.VirtualTPM{},
+				Operation: types.VirtualDeviceConfigSpecOperationAdd,
+			},
+		},
+	}
+
+	task, err := vm.Reconfigure(ctx, spec)
 	if err != nil {
+		return err
+	}
+	if err = task.Wait(ctx); err != nil {
 		return err
 	}
 
@@ -58,6 +95,7 @@ func (cmd *add) Run(ctx context.Context, f *flag.FlagSet) error {
 		return err
 	}
 
+	d := &types.VirtualTPM{}
 	devices = devices.SelectByType(d)
 
 	name := devices.Name(devices[len(devices)-1])
